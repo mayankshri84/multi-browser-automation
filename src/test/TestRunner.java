a144153c9eaed05d54e000326db58553a50341ad
Mayank Shrivastava <mayankshri84@gmail.com>
2018-10-09 00:50:30 +0530

    cucumber setup

---
+++ src/test/TestRunner.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features = "src/features"
+	,glue={"StepDefinition"}
+	,tags={"@Login"}
+)
+
+public class TestRunner {
+
+}
--- src/utils/GeneralUtils.java
+++ src/utils/GeneralUtils.java
@@ -4,10 +4,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import com.google.gson.*;
+
+
 public class GeneralUtils {
 	
+	
+	public static void main(String[] args) {
+		new GeneralUtils().jsonReader("./tests.json", "IE");
+	}
+	
 	public void jsonReader(String filePath, String key){
-		readFile(filePath);
+		String json = readFile(filePath);
+		JsonParser jsonParser = new JsonParser();
+		JsonElement ele = jsonParser.parse(json);
+		System.out.println(ele);
+		System.out.println(ele.getAsJsonObject().getAsJsonArray(key));
+		
 	}
 	
 	
